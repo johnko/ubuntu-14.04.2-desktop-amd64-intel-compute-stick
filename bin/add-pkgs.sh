d4ff4c8c9d7fdfb6182622cf6547445c6439519d
johnko <git@johnko.ca>
2015-11-29 01:23:44 -0500

    install pkg

--- bin/add-pkgs.sh
+++ bin/add-pkgs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -e ./pkg ]; then
+	sudo install -m 755 ./pkg /usr/bin/pkg
+fi
+
 sudo -E apt-get update
 #sudo -E apt-get install linux-generic-lts-utopic
 sudo -E apt-get install git tmux tasksel openssh-client
